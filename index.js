b17e4463b32826d68d15e9b51f22368a66914010
Josef Benninghoff <jsbenning4@gmail.com>
2018-05-04 01:07:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ var customerName = "bob";
 function upperCaseCustomerName(customerName){
   return customerName.toUpperCase();
 }
+
+function setBestCustomer(){
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "billy";
+}
